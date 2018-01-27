d5aa2d36523718fa4efcd5dd778df121bfe04b6b
k4m35h <1234.kamesh@gmail.com>
2018-01-27 21:24:27 +0530

    Update koops_25.c

--- share/DV/koops_25.c
+++ share/DV/koops_25.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+
+void sort(int a[],int n) { 
+   int i,j;
+   for(i=0;i<n-1;i++) for(j=0;j<n-i-1;j++) if(a[j]>a[j+1])swap(&a[j],&a[j+1]);
+}
+
+void swap(int *p,int *q) {
+   int t;
+   t=*p;*p=*q; *q=t;
+}
+
 int main()
 {
-    int total=0;
-    int n,t;
+    int n,t,data[100];
     scanf("%d",&n);
-    for(int i=n;i>0;i--){
-        scanf("%d",&t);
-        total += t; 
-    }
-    printf("%d",total/n);
+    for(int i=0;i<n;i++) scanf("%d",&data[i]); 
+    sort(data,n);
+    n = (n+1)/2;
+    printf("%d",data[n-1]);
     return 0;
 }
