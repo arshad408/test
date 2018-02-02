363a62cced6628308b0da70401b46b25e8776b99
k4m35h <1234.kamesh@gmail.com>
2018-02-02 16:28:27 +0530

    Create composite.c

---
+++ c/composite.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,i,task=0;
+    scanf("%d",&a);
+    for(i=2;i<a;i++)
+        if(a%i == 0){
+            task=1;
+            break;
+        }
+    printf((task)?"yes":"no");
+}
